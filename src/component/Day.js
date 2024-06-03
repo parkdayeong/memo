eafdfe0b91d8bd03e045bcc99b952a33eeaba378
parkdayeong <45747299+parkdayeong@users.noreply.github.com>
2024-06-03 16:09:12 +0900

    240603-customhook-fetch.js

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Word from './Word';
+import useFetch from '../hooks/useFetch';
 
 const Day = () => {
   const day = useParams().day;
-
-  const [words, setWords] = useState([]);
-
-  useEffect(() => {
-    fetch(`http://localhost:3001/words?day=${day}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        // console.log(data);
-        setWords(data);
-      });
-  }, [day]);
+  const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
   return (
     <>
