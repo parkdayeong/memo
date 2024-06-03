667f05e001c4d236dd281f51ab7fc17435c3e169
parkdayeong <45747299+parkdayeong@users.noreply.github.com>
2024-06-03 14:01:34 +0900

    240603-add router, emptypage

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Headers from './component/Header';
 import DayList from './component/DayList';
 import Day from './component/Day';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import EmptyPage from './component/EmptyPage';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Headers />
-      <DayList />
-      <Day />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Headers />
+        <Routes>
+          <Route path='/' element={<DayList />} />
+          <Route path='/day/:day' element={<Day />} />
+          <Route path='*' element={<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import dummy from '../db/data.json';
+import { Link } from 'react-router-dom';
 
 const DayList = () => {
   console.log(dummy);
   return (
     <ul className='list_day'>
       {dummy.days.map((day) => {
-        return <li key={day.id}>Day {day.day}</li>;
+        return (
+          <li key={day.id}>
+            <Link to={`/day/${day.day}`}>Day {day.day}</Link>
+          </li>
+        );
       })}
     </ul>
   );
